2363ead6488091685262399ce4a54344cce9d05f
ProGoldfish <60945731+ProGoldfish@users.noreply.github.com>
2020-02-12 10:35:46 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,3 @@ from Save import save
 username = ""
 while len(username) < 1 or len(username) > 8 or any(ext in username for ext in ["0","1","2","3","4","5","6","7","8","9"," "]):
 	username = input("Enter your username: (no numbers or spaces)\n> ").capitalize()
-filename = username
-print(filename)
-input(__import__(filename))
-input(seed)
