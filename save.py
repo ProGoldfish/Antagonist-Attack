aa63ca109b6ccf2119633be8509c65550088e064
ProGoldfish <60945731+ProGoldfish@users.noreply.github.com>
2020-02-12 10:24:24 +0000

    team jake

--- save.py
+++ save.py
@@ -1,5 +1,5 @@
 def save(username,seed,savedata,maxhp,playerhp,attackmax,attackmin,armour):
-    filename = "Save Files/"+ username + ".py"
+    filename = username + ".py"
     file = open(filename, "w")
     data = ["seed = "+ str(seed), "\n", "savedata = "+ str(savedata), "\n", "playerhp = "+ str(playerhp)]
     for i in range(len(data)):
