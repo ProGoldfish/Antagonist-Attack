02f734ce240c1447f76e770faa90e540caebd11b
ProGoldfish <60945731+ProGoldfish@users.noreply.github.com>
2020-02-12 11:19:13 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,5 +5,5 @@ winsound.PlaySound("Sounds/ph.wav", winsound.SND_ASYNC)
 username = ""
 while len(username) < 1 or len(username) > 8 or any(ext in username for ext in ["0","1","2","3","4","5","6","7","8","9"," "]):
 	username = input("Enter your username: (no numbers or spaces)\n> ").capitalize()
-filename = username+ ".txt"
+filename = "Save Files/"+ username+ ".txt"
 file = open(filename, "r")
