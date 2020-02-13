05881774745bbf47450ccc1c88c2ccd8c7843ed6
ProGoldfish <60945731+ProGoldfish@users.noreply.github.com>
2020-02-13 15:05:16 +0000

    made it run after robby broke it

--- Main.py
+++ Main.py
@@ -5,30 +5,30 @@ from Character import Character, Enemy, Friend
 from Item import Item
 
 def Read(username):
-	filename = "Save Files/"+ username+ ".txt"
-	file = open(filename, "r")
-	data = file.readlines()
-	file.close()
-	seed = data[0]
-	seed = seed.replace("\n","")
-	seed = seed.replace("[","")
-	seed = seed.replace("]","")
-	seed = seed.replace(",","")
-	seed = seed.replace(" ","")
-	list(seed)
-	savedata = data[1]
-	savedata = savedata.replace("\n","")
-	savedata = savedata.replace("[","")
-	savedata = savedata.replace("]","")
-	savedata = savedata.replace(",","")
-	savedata = savedata.replace(" ","")
-	list(savedata)
-	playerhp = data[2]
-	playerhp = playerhp.replace("\n","")
-	return seed, savedata, playerhp
+        filename = "Save Files/"+ username+ ".txt"
+        file = open(filename, "r")
+        data = file.readlines()
+        file.close()
+        seed = data[0]
+        seed = seed.replace("\n","")
+        seed = seed.replace("[","")
+        seed = seed.replace("]","")
+        seed = seed.replace(",","")
+        seed = seed.replace(" ","")
+        list(seed)
+        savedata = data[1]
+        savedata = savedata.replace("\n","")
+        savedata = savedata.replace("[","")
+        savedata = savedata.replace("]","")
+        savedata = savedata.replace(",","")
+        savedata = savedata.replace(" ","")
+        list(savedata)
+        playerhp = data[2]
+        playerhp = playerhp.replace("\n","")
+        return seed, savedata, playerhp
 
 def TitleScreen():
-        winsound.PlaySound("Sounds/startup.wav", winsound.SND_ASYNC)
+	winsound.PlaySound("Sounds/startup.wav", winsound.SND_ASYNC)
 	print("                                              .-'''-.                                ")
 	time.sleep(0.05)
 	print("                                             '   _    \\                              ")
@@ -87,8 +87,11 @@ def Lobby():
         while location not in ["blacksmith","store","armoury","help"]:
                 location = input("You are welcomed back into the humbling town, the towns workers are greeting you with wonderful smiles, the Blacksmith(blacksmith), Merchant(store), Armourer(armoury)\n> ").lower()
         if location == "blacksmith":
+                input("")
         if location == "store":
+                input("")
         if location == "armoruy":
+                input("")
         if location == "help":
                 input("Here you can explore and upgrade your utility within the local town. The blacksmith is for Upgrading weapons, store if for buying consumables, whilst armoury is for buying new weapons of higher tier") 
                 
