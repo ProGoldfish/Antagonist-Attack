77000662a8202d5462d217116218d3a651d18704
ProGoldfish <60945731+ProGoldfish@users.noreply.github.com>
2020-02-13 18:16:19 +0000

    added functions for menu and stuff

--- Character Creation.py
+++ Character Creation.py
@@ -3,6 +3,8 @@ from Save import Save
 seed = []
 savedata = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+def NewPage():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
 username = ""
 
@@ -16,39 +18,23 @@ for i in range(200):
 
 done = "n"
 while done == "n":
+    NewPage()
     while len(username) < 1 or len(username) > 15 or any(ext in username for ext in ["0","1","2","3","4","5","6","7","8","9"," "]):
         username = input("Please enter a username that you will remember: (no numbers or spaces - usernames are not case sensitive)\n> ").capitalize()
 
     while savedata[0] not in ["1","2","3"]:
         savedata[0] = input("\nChoose a class:\nArcher (1)\nWarrior (2)\nAlchemist (3)\n> ")
-
     if savedata[0] == "1":
-        maxhp = 100
-        playerhp = 100
-        attackmax = 25
-        attackmin = 20
-        armour = 1
-        print("\nYou have chosen Archer!\n\nA ranger able to make full use of the long range weapons.\nYou shoot down your enemies but be careful not to get hit!\n\nHealth: {}\nMaximum Damage: {}\nMinimum Damage: {}\nArmour: {}".format(maxhp, attackmax, attackmin, armour)) 
+        print("\nYou have chosen Archer!\n\nA ranger able to make full use of the long range weapons.\nYou shoot down your enemies but be careful not to get hit!\n\nHealth: 100\nMaximum Damage: 25\nMinimum Damage: 20\nArmour: 1") 
     if savedata[0] == "2":
-        maxhp = 125
-        playerhp = 125
-        attackmax = 25
-        attackmin = 20
-        armour = 1.25
-        print("\nYou have chosen Warrior!\n\nA veteran fighter able to take full bulks of damage.\nYou even dish out a fair amount your self!\n\nHealth: {}\nMaximum Damage: {}\nMinimum Damage: {}\nArmour: {}".format(maxhp, attackmax, attackmin, armour))
+        print("\nYou have chosen Warrior!\n\nA veteran fighter able to take full bulks of damage.\nYou even dish out a fair amount your self!\n\nHealth: 125\nMaximum Damage: 20\nMinimum Damage: 15\nArmour: 1.25")
     if savedata[0] == "3":
-        maxhp = 75
-        playerhp = 75
-        attackmax = 15
-        attackmin = 10
-        armour = 0.9
-        print("\nYou have chosen Alchemist!\n\nA magical scientist able to make full use of the earthly elements.\nYou poison your oppoments and stall them out with insane healing capabilties!\n\nHealth: {}\nMaximum Damage: {}\nMinimum Damage: {}\nArmour: {}".format(maxhp, attackmax, attackmin, armour))
-
+        print("\nYou have chosen Alchemist!\n\nA magical scientist able to make full use of the earthly elements.\nYou poison your oppoments and stall them out with insane healing capabilties!\n\nHealth: 75\nMaximum Damage: 15\nMinimum Damage: 10\nArmour: 0.9")
     savedata[0] = int(savedata[0])
     done = input("\nAre you sure you want this name and class, "+ username+"? (y/n)\n> ").lower()
     if done == "n":
         savedata[0] = 0
         username = ""
 
-Save(username, seed, savedata, playerhp)
+Save(username, seed, savedata, 0)
 os.startfile("Main.py")
--- Main.py
+++ Main.py
@@ -5,27 +5,36 @@ from Character import Character, Enemy, Friend
 from Item import Item
 
 def Read(username):
-        filename = "Save Files/"+ username+ ".txt"
-        file = open(filename, "r")
-        data = file.readlines()
-        file.close()
-        seed = data[0]
-        seed = seed.replace("\n","")
-        seed = seed.replace("[","")
-        seed = seed.replace("]","")
-        seed = seed.replace(",","")
-        seed = seed.replace(" ","")
-        list(seed)
-        savedata = data[1]
-        savedata = savedata.replace("\n","")
-        savedata = savedata.replace("[","")
-        savedata = savedata.replace("]","")
-        savedata = savedata.replace(",","")
-        savedata = savedata.replace(" ","")
-        list(savedata)
-        playerhp = data[2]
-        playerhp = playerhp.replace("\n","")
-        return seed, savedata, playerhp
+    filename = "Save Files/"+ username+ ".txt"
+    file = open(filename, "r")
+    data = file.readlines()
+    file.close()
+    seed = data[0]
+    seed = seed.replace("\n","")
+    seed = seed.replace("[","")
+    seed = seed.replace("]","")
+    seed = seed.replace(",","")
+    seed = seed.replace(" ","")
+    list(seed)
+    savedata = data[1]
+    savedata = savedata.replace("\n","")
+    savedata = savedata.replace("[","")
+    savedata = savedata.replace("]","")
+    savedata = savedata.replace(",","")
+    savedata = savedata.replace(" ","")
+    list(savedata)
+    playerhp = data[2]
+    playerhp = playerhp.replace("\n","")
+    return seed, savedata, playerhp
+
+def PlayerSound():
+    global playerclass
+    if playerclass == "Archer":
+        winsound.PlaySound("Sounds/arrow.wav", winsound.SND_ASYNC)
+    if playerclass == "Warrior":
+        winsound.PlaySound("Sounds/swords.wav", winsound.SND_ASYNC)
+    if playerclass == "Alchemist":
+        winsound.PlaySound("Sounds/spell.wav", winsound.SND_ASYNC)
 
 def NewPage():
 	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -83,31 +92,53 @@ def TitleScreen():
     print(" `--'  `\"  `'-'    `'-'  `--'  `\"             '------'  '---`''--'                   ")
     time.sleep(0.05)
 
-def Lobby():
-        global playerclass
-        global username
+def Town():
+    global playerclass
+    global username
+    NewPage()
+    location = ""
+    print("You are welcomed back into the humbling town.\nThe towns workers are greeting you with wonderful smiles.\n")
+    while location != "leave":
+        while location not in ["blacksmith","merchant","armourer","help","leave"]:
+            location = input("The Blacksmith (\"blacksmith\"), Merchant (\"merchant\") and Armourer (\"armourer\") are here. (type \"leave\" to leave)\n> ").lower()
+            NewPage()
+            if location == "blacksmith":
+                    location = ""
+                    input("blacksmith\n >")
+                    NewPage()
+            if location == "merchant":
+                    location = ""
+                    input("merchant\n >")
+                    NewPage()
+            if location == "armourer":
+                    location = ""
+                    input("armourer\n >")
+                    NewPage()
+            if location == "help":
+                    location = ""
+                    input("\nHere you can explore and upgrade your utility within the local town.\n\nThe blacksmith is for upgrading weapons.\nThe store if for buying consumables.\nThe armourer is for Upgrading weapons.\n(enter to continue)\n> ")
+                    NewPage()
+    Menu()
+
+def Menu():
+    playerinput = ""
+    while playerinput not in ["dungeon","town","quit"]:
         NewPage()
-        location = ""
-        print("You are welcomed back into the humbling town.\nThe towns workers are greeting you with wonderful smiles.\n")
-        while location != "leave":
-            while location not in ["blacksmith","merchant","armourer","help","leave"]:
-                location = input("The Blacksmith (\"blacksmith\"), Merchant (\"merchant\") and Armourer (\"armourer\") are here. (type \"leave\" to leave)\n> ").lower()
-                if location == "blacksmith":
-                        location = ""
-                        input("")
-                        NewPage()
-                if location == "merchant":
-                        location = ""
-                        input("")
-                        NewPage()
-                if location == "armourer":
-                        location = ""
-                        input("")
-                        NewPage()
-                if location == "help":
-                        location = ""
-                        input("\nHere you can explore and upgrade your utility within the local town.\n\nThe blacksmith is for upgrading weapons.\nThe store if for buying consumables.\nThe armourer is for Upgrading weapons.\n(enter to continue)\n> ")
-                        NewPage()
+        playerinput = input("What would you like to do now my mighty "+ playerclass+ "?\n(A \"dungeon\", back to the \"town\" or \"quit\" the game)\n> ").lower()
+    if playerinput == "dungeon":
+        playerinput = ""
+        Dungeon()
+    if playerinput == "town":
+        playerinput = ""
+        Town()
+    if playerinput == "quit":
+        playerinput = ""
+
+def Dungeon():
+    NewPage()
+    input("dungeon\n> ") #robby put the dungeon code here pls. you may have to global all the stat variables
+
+    Menu()
 
 TitleScreen()
 username = ""
@@ -119,15 +150,47 @@ seed, savedata, playerhp = Read(username)
 
 if savedata[0] == "1":
     playerclass = "Archer"
-    winsound.PlaySound("Sounds/arrow.wav", winsound.SND_ASYNC)
+    maxhp = 100
+    playerhp = 100
+    attackmax = 25
+    attackmin = 20
+    armour = 1
+    PlayerSound()
 if savedata[0] == "2":
     playerclass = "Warrior"
-    winsound.PlaySound("Sounds/swords.wav", winsound.SND_ASYNC)
+    maxhp = 125
+    playerhp = 125
+    attackmax = 20
+    attackmin = 15
+    armour = 1.25
+    PlayerSound()
 if savedata[0] == "3":
     playerclass = "Alchemist"
-    winsound.PlaySound("Sounds/spell.wav", winsound.SND_ASYNC)
-print("\nWelcome "+ username+ "... My finest "+ playerclass+ "!\nIf you need a list of commands, type \"help\" at any point.\n")
+    maxhp = 75
+    playerhp = 75
+    attackmax = 15
+    attackmin = 10
+    armour = 0.9
+    PlayerSound()
 
-Lobby()
+NewPage()
+print("\nWelcome "+ username+ "... My finest "+ playerclass+ "!")
+playerinput = input("If you need a list of commands, type \"help\" at any point.\n(enter to continue)\n> ").lower()
+if playerinput == "help":
+    NewPage()
+    playerinput = ""
+    playerinput = input("You are literally on the welcome screen, "+ username+ "...\nThere is nothing to help you with.\n(enter to continue)\n> ").lower()
+    if playerinput == "help":
+        NewPage()
+        playerinput = ""
+        playerinput = input("Ok you can stop now, "+ username+ "...\n(enter to continue)\n> ").lower()
+        if playerinput == "help":
+            NewPage()
+            playerinput = ""
+            playerinput = input("Ok, I've had enough of this...\n(enter to continue)\n> ").lower()
+            while playerinput == "help":
+                NewPage()
+                playerinput = ""
+                playerinput = input("(enter to continue)\n> ").lower()
 
-input("end") #stops program from ending instantly
+Town()
